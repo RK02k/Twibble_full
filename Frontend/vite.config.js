d4038ea0d74dbe88e426bc28d95fe84564d2d888
Raj <121766917+RK02k@users.noreply.github.com>
2024-07-13 03:07:36 +0530

    Last try

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,42 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['scriptjs']
+//     }
+//   }
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
+  // Fix large chunks issue due to node_modules
   build: {
     rollupOptions: {
-      external: ['scriptjs']
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 });

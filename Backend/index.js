84f39132827763ae4e9b7da5d2fe52956ba9b060
Raj <121766917+RK02k@users.noreply.github.com>
2024-07-13 15:25:42 +0530

    ujm

--- Backend/index.js
+++ Backend/index.js
@@ -14,23 +14,19 @@ const saltRounds = 10;
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ['https://courageous-kringle-bab0ed.netlify.app/', 'https://twibbleproject.vercel.app/'];
+const allowedOrigins = ['https://courageous-kringle-bab0ed.netlify.app', 'https://twibbleproject.vercel.app'];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   credentials: true
-// }));
-
-// app.use(cors({credentials: true}));
-
-app.use(cors())
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);  // Allow requests with no origin (like mobile apps or curl requests)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 

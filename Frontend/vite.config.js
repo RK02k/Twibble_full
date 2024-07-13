7973d710c134e3f7c5207270fa7d4db8fdbe2411
Raj <121766917+RK02k@users.noreply.github.com>
2024-07-13 16:32:25 +0530

    Go on

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import vitePluginRequire from 'vite-plugin-require';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePluginRequire.default()
-  ],
-  build: {
-    outDir: 'dist',
-  }
-});
+  plugins: [react()],
+})
